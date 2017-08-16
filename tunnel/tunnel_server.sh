03add1dda6fccfd96ebbcd23b1c9a4c5bd060e8e
Jerome Pinguet <jerome@jerome.cc>
2017-08-16 06:11:01 +0200

    fix authorized_keys permissions

--- tunnel/tunnel_server.sh
+++ tunnel/tunnel_server.sh
@@ -10,7 +10,7 @@ mkdir /home/$USER/.ssh
 chmod 700 /home/$USER/.ssh
 chown $USER:$USER /home/$USER/.ssh
 touch /home/$USER/.ssh/authorized_keys
-chmod 644 /home/$USER/.ssh/authorized_keys
+chmod 600 /home/$USER/.ssh/authorized_keys
 chown $USER:$USER /home/$USER/.ssh/authorized_keys
 
 # use termbin.com
